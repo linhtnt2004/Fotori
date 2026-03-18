92e4f4b39fecacb4c2e3a5e58771cf4d441bd458
Yuno-debug <duchungtran2004@gmail.com>
2026-03-19 04:38:50 +0700

    fix: set server timezone to Asia/Ho_Chi_Minh (fixes all date/time issues)

--- src/main/java/com/example/fotori/FotoriApplication.java
+++ src/main/java/com/example/fotori/FotoriApplication.java
@@ -5,10 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableAsync
 @EnableJpaAuditing
 @SpringBootApplication
 public class FotoriApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FotoriApplication.class, args);
